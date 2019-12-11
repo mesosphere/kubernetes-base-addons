b3939ebb590f0c20f282f8f2f310de3cdb046f5b
alejandroEsc <jaescobar.cell@gmail.com>
2019-12-11 15:21:19 -0800

    was able to get kommander to deploy

--- test/addons_test.go
+++ test/addons_test.go
@@ -1,9 +1,19 @@
 package test
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"fmt"
 	"io/ioutil"
+	"math/big"
+	"os"
+	"os/exec"
+	"path"
 	"testing"
+	"time"
 
 	"github.com/blang/semver"
 	"gopkg.in/yaml.v2"
@@ -74,13 +84,23 @@ func TestKommanderGroup(t *testing.T) {
 
 func testgroup(t *testing.T, groupname string) error {
 	t.Logf("testing group %s", groupname)
-	cluster, err := kind.NewCluster(semver.MustParse(defaultKubernetesVersion))
+
+	version, err := semver.Parse(defaultKubernetesVersion)
+	if err != nil {
+		return err
+	}
+
+	cluster, err := kind.NewCluster(version)
 	if err != nil {
 		return err
 	}
 	defer cluster.Cleanup()
 
 	if err := temp.DeployController(cluster); err != nil {
+		return err
+	}
+
+	if err:= deployCertManagerCA(cluster); err != nil {
 		return err
 	}
 
@@ -148,4 +168,71 @@ func findUnhandled() ([]v1beta1.AddonInterface, error) {
 	}
 
 	return unhandled, nil
+}
+
+func deployCertManagerCA(cluster test.Cluster) error {
+	if err := kubectl("--kubeconfig", cluster.ConfigPath(), "create", "namespace", "cert-manager"); err != nil {
+		return err
+	}
+
+	// create secret
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	ca := &x509.Certificate{
+		SerialNumber: big.NewInt(1653),
+		Subject: pkix.Name{
+			Organization:  []string{"d2iq"},
+			Country:       []string{"US"},
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(10, 0, 0),
+		IsCA:                  true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+	}
+
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return err
+	}
+	pub := &priv.PublicKey
+	ca_b, err := x509.CreateCertificate(rand.Reader, ca, ca, pub, priv)
+
+	certPath := path.Join(wd, "ca.crt")
+	keyPath := path.Join(wd, "ca.key")
+
+
+	certOut, err := os.Create(certPath)
+	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: ca_b})
+	certOut.Close()
+
+	// Private key
+	keyOut, err := os.OpenFile(keyPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
+	keyOut.Close()
+
+	// create kubernetes-root-ca secret
+	if err := kubectl("--kubeconfig", cluster.ConfigPath(), "create", "secret", "tls", "kubernetes-root-ca", "--namespace=cert-manager", fmt.Sprintf("--cert=%s",certPath), fmt.Sprintf("--key=%s", keyPath)); err != nil {
+		return err
+	}
+
+	// create konvoyconfig-kubeaddons configmap
+	if err := kubectl("--kubeconfig", cluster.ConfigPath(), "create", "configmap", "konvoyconfig-kubeaddons", "--namespace=kubeaddons", "--from-literal=clusterHostname=kommander.example.com"); err != nil {
+		return err
+	}
+
+	defer os.Remove(certPath)
+	defer os.Remove(keyPath)
+	return nil
+}
+
+func kubectl(args ...string) error {
+	cmd := exec.Command("kubectl", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
