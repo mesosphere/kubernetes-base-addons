7f902f6adaadd6c8dc319f69e09191ef1b4cb3da
Senthil Kumaran <senthil@uthcode.com>
2020-03-30 22:04:46 -0700

    update test run tests.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -6,6 +6,7 @@ git fetch
 echo "INFO: the following test groups will be run:"
 go run -tags experimental scripts/test-wrapper.go
 
-for g in $(go run scripts/test-wrapper.go); do
-	go test -timeout 30m -race -v -run $g;
+for g in $(go run -tags experimental scripts/test-wrapper.go)
+do
+    go test -tags experimental -timeout 60m -race -v -run $g
 done
