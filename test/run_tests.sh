8b726e727c008992f698b42afa8ca9408df2bbff
Senthil Kumaran <senthil@uthcode.com>
2020-04-01 16:08:14 -0700

    Start with a single test.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -4,9 +4,11 @@ set -euxo pipefail
 git fetch
 
 echo "INFO: the following test groups will be run:"
-go run -tags experimental scripts/test-wrapper.go
+#go run -tags experimental scripts/test-wrapper.go
 
-for g in $(go run -tags experimental scripts/test-wrapper.go)
-do
-    go test -tags experimental -timeout 60m -race -v -run $g
-done
+go test -tags experimental -timeout 60m -race -v -run TestGeneralGroup
+
+#for g in $(go run -tags experimental scripts/test-wrapper.go)
+#do
+#    go test -tags experimental -timeout 60m -race -v -run $g
+#done
