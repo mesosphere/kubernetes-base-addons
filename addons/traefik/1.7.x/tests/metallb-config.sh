bbe962dbba0d23db9c187b16f0428ea16227b355
Joe Julian <me@joejulian.name>
2020-04-23 15:18:04 -0700

    get kuttl working to install traefik from the addon

---
+++ addons/traefik/1.7.x/tests/metallb-config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+JP='{.status.addresses[?(.type == "InternalIP")].address}'
+NODEIP=$(kubectl get node kind-control-plane -o jsonpath="${JP}")
+SLASH24=$(sed 's@\.[0-9]\+$@@' <<< ${NODEIP})
+kubectl -n kubeaddons create configmap metallb-config --from-literal=config='{"address-pools":[{"name":"default","protocol":"layer2","addresses":["'${SLASH24}'.200-'${SLASH24}'.250"]}]}'
+
