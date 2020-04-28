0d68e5f215d8f837c74483390c8330e4b9a0dd95
Joe Julian <me@joejulian.name>
2020-04-28 11:54:31 -0700

    add test

---
+++ addons/traefik/1.7.x/tests/proxyexec.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+SERVER=$(kubectl -n kubeaddons get svc traefik-kubeaddons \
+    -o jsonpath="{.status.loadBalancer.ingress[0].ip}{.status.loadBalancer.ingress[0].hostname}")
+SECRET=$(kubectl -n default get serviceaccount kuttlaccount -o jsonpath="{ .secrets[0].name }")
+TOKEN=$(kubectl -n default get secret "${SECRET}" -o go-template="{{.data.token | base64decode }}")
+kubectl config set-credentials kuttl --token="${TOKEN}"
+kubectl config set-context kuttl --cluster=cluster --user=kuttl
+kubectl config use-context kuttl
+kubectl -v 9 -s https://"${SERVER}"/testpath run --stdin=true --restart=Never --image busybox proxyexec echo SUCCESS
