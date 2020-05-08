979be44fa9423276608f5e8c6494732758704c7c
Joe Julian <me@joejulian.name>
2020-05-08 15:24:49 -0700

    do not pollute kubeconfig

--- addons/traefik/1.7.x/tests/exec-test.sh
+++ addons/traefik/1.7.x/tests/exec-test.sh
@@ -3,7 +3,10 @@ SERVER=$(kubectl -n kubeaddons get svc traefik-kubeaddons \
     -o jsonpath="{.status.loadBalancer.ingress[0].ip}{.status.loadBalancer.ingress[0].hostname}")
 SECRET=$(kubectl -n default get serviceaccount kuttlaccount -o jsonpath="{ .secrets[0].name }")
 TOKEN=$(kubectl -n default get secret "${SECRET}" -o go-template="{{.data.token | base64decode }}")
-kubectl config set-credentials kuttl --token="${TOKEN}"
-kubectl config set-context kuttl --cluster=cluster --user=kuttl
-kubectl config use-context kuttl
-kubectl -v 9 -s https://"${SERVER}"/testpath exec -ti testpod -- echo SUCCESS 2>.kube/kubectl.log
+TMPKUBECONFIG=$(mktemp)
+cp "${KUBECONFIG}" "${TMPKUBECONFIG}"
+kubectl --kubeconfig "${TMPKUBECONFIG}" config set-credentials kuttl --token="${TOKEN}"
+kubectl --kubeconfig "${TMPKUBECONFIG}" config set-context kuttl --cluster=cluster --user=kuttl
+kubectl --kubeconfig "${TMPKUBECONFIG}" config use-context kuttl
+kubectl --kubeconfig "${TMPKUBECONFIG}" -v 9 -s https://"${SERVER}"/testpath exec -ti testpod -- echo SUCCESS 2>.kube/kubectl.log
+rm "${TMPKUBECONFIG}"
