72fe244c8d33ac648ba30e61547a2f8f7103b970
Sebastian Brandt <sbrandt@d2iq.com>
2020-08-06 16:24:01 +0200

    chore: test updates
    - evil time.Sleep to get tests passed once now
    - updated scripts to use konvoy 1.5.0, static atm
    - updated script to take uname
    - updated go modules
    
    Co-Authored-By: Shane Utt <shaneutt@users.noreply.github.com>

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -8,7 +8,7 @@ set -euxo pipefail
 cd "$(dirname "$0")"
 
 echo "Setup Konvoy"
-source ./scripts/setup-konvoy.sh v1.5.0-beta.4
+source ./scripts/setup-konvoy.sh v1.5.0
 
 echo "git fetch branches."
 git fetch
