eb8f61503de4b51c0366d79a355497026b7f83a0
faiq <faiqrazarizvi@gmail.com>
2021-02-10 18:41:43 -0800

    Konvoy parallel attempt

--- test/test-aws.sh
+++ test/test-aws.sh
@@ -3,13 +3,30 @@ set -euo pipefail
 
 branch=${1:-master}
 
-./scripts/setup-konvoy.sh
-
 echo "INFO: the following test groups will be run:"
 tests=$(go run -tags experimental scripts/test-wrapper.go origin ${branch} | grep '^Test'  | grep 'TestAwsGroup\|TestElasticsearchGroup\|TestIstioGroup')
 echo ${tests}
 
+pids=()
 for g in ${tests}
 do
-    go test -tags experimental -timeout 60m -race -v -run $g -kba-branch ${branch}
+    go test -tags experimental -timeout 60m -race -v -run $g -kba-branch ${branch} > $g 2>&1 &
+    pids+=($!)
+done
+
+exits=()
+for pid in ${pids[*]}; do
+  wait $pid || EXIT_CODE=$?
+  exits+=($EXIT_CODE)
+  if [ $DISPATCH_CI = "true" ]; then
+    cp Test* /workspace/output
+  fi
+done
+
+echo ${exits[*]}
+
+for e in ${exits[*]}; do
+  if [ ${e} -ne 0 ]; then
+    exit $e
+  fi
 done
