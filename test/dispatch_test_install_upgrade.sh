8ca616cf847f8d169d88930bcdc8c5f8d5f3fb31
Alejandro Escobar <jaescobar.cell@gmail.com>
2021-02-16 13:19:07 -0800

    Rework tests to be more parallel (#860)
    * chore: split and parallize install and upgrade jobs.
    
    * run in shell
    
    * remove back-ticks
    
    * Fixes makefile target for Dispatch
    
    * move test to local bash script
    
    * set kubeconfig to avoid an OOM
    
    * multiple fixes to the makefile and dispatch
    
    * multiple fixes to the makefile and dispatch
    
    * fixing naming and cleanup
    
    * fixing naming and cleanup
    
    * Debug and ignore dispatchfile
    
    * fixing double quotes
    
    * create resources on dispath kind cluster
    
    * create resources on dispath kind cluster
    
    * copy cert code
    
    * Try to fetch ca.crt and key from remote cluster
    
    * rework command
    
    * small fix
    
    * Import task result for upgrade
    
    * Should work now
    
    * Cleans up the test install upgrade script
    
    * Fixes tests for install and upgrade in dispatch
    
    * Remove unused util file
    
    * Creates a seperate task for AWS groups
    
    * Change file mode for aws addons
    
    * Use a better name for container step
    
    * refactorr and clean addon_test.go
    
    * split aws into install and upgrade
    
    * check out KBA_BRANCH in 2 ways for debug
    
    * Fix Dispatchfile
    
    * Istio groups to their own cluster on aws
    
    * Fix istio group name
    
    * Debug: just run istio test
    
    * move istio group to konvoy testing
    
    * turn on aws testing again
    
    * Ignore the Istio group in Dispatch tests
    
    * Konvoy parallel attempt
    
    * Fixes OOMKilled Issue and reworks copy artifacts
    
    * Download konvoy beforehand
    
    * More memory for parallel tests
    
    * copy the konvoy to the test
    
    * test-aws.sh changes
    
    * Print test results at the end
    
    * add pushd and popd instead of using cd
    
    Co-authored-by: faiq <faiqrazarizvi@gmail.com>

---
+++ test/dispatch_test_install_upgrade.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+branch=${1:-master}
+
+# if we're in dispatch - create the secrets on the container running in the pod with exec
+if [[ ! -z "${CLAIM_NAME}" ]]; then
+    kubectl --kubeconfig ${KBA_KUBECONFIG} create namespace cert-manager || true
+    KIND_POD_NAME=$(kubectl get clusterclaim $CLAIM_NAME | tail +2 | awk '{print $2}')
+    kubectl exec -n dispatch $KIND_POD_NAME -- bash -c "docker ps -aq | xargs -I{} docker exec {} cat /etc/kubernetes/pki/ca.crt"  >> ca.crt
+    kubectl exec -n dispatch $KIND_POD_NAME -- bash -c "docker ps -aq | xargs -I{} docker exec {} cat /etc/kubernetes/pki/ca.key"  >> ca.key
+    kubectl --kubeconfig ${KBA_KUBECONFIG} create secret tls kubernetes-root-ca --namespace=cert-manager --cert=ca.crt --key=ca.key --dry-run -o yaml  | kubectl --kubeconfig ${KBA_KUBECONFIG} apply -f -  || true
+fi
+
+
+echo "INFO: the following test groups will be run:"
+tests=$(go run -tags experimental scripts/test-wrapper.go origin ${branch} | (egrep '^Test' || true) | grep -v 'TestAwsGroup\|TestElasticsearchGroup\|TestIstioGroup')
+echo ${tests}
+
+for g in ${tests}
+do
+    go test -tags experimental -timeout 60m -race -v -run $g -kba-branch ${branch}
+done
