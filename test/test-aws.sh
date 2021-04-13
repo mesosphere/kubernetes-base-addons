bc8b98553a7713013b40ae2925dc22565e8a6055
Faiq Raza <faiq@users.noreply.github.com>
2021-04-12 23:07:24 -0600

    Faiq/release/3 dispatch backport (#1050)
    * Backport tests to release/3
    
    * Remove dispatch integration test which is covered by test install and test install aws
    
    * Adds make targets and scripts
    
    * Adds github cli bin after checkout error
    
    * Adds dispatch test script
    
    * Echo branch, which shouldbe release/3
    
    * hard code release/3 branch
    
    * Fetch the missing branch

---
+++ test/test-aws.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -eo pipefail
+
+branch=${1:-release/3}
+
+echo "INFO: the following test groups will be run:"
+tests=$(go run -tags experimental scripts/test-wrapper.go origin "${branch}" | grep '^Test'  | grep 'TestAwsGroup\|TestElasticsearchGroup\|TestIstioGroup') && echo ${tests}
+
+pids=()
+for g in ${tests}
+do
+    go test -tags experimental -timeout 60m -race -v -run "${g}" -kba-branch "${branch}" > "${g}.log" 2>&1 &
+    pids+=($!)
+done
+
+exits=()
+EXIT_CODE=0
+for pid in ${pids[*]}; do
+  wait "${pid}" || EXIT_CODE=$?
+  exits+=("${EXIT_CODE}")
+  if [ "${DISPATCH_CI}" = "true" ]; then
+    cat Test*
+    cp Test* "${ARTIFACTS_DIR}"
+  fi
+done
+
+echo "${exits[*]}"
+
+for e in ${exits[*]}; do
+  if [ "${e}" -ne 0 ]; then
+    exit "${e}"
+  fi
+done
