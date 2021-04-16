ff4e5a1f213bbb62a060ab24294144d1850c91b3
Joe Julian <me@joejulian.name>
2021-04-15 18:13:52 -0700

    use github cli to determine the merge target

--- test/dispatch_test_install_upgrade.sh
+++ test/dispatch_test_install_upgrade.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-branch=${1:-release/4}
-git fetch origin ${branch}
+if [[ ${DISPATCH_BUILD_NAME#pr-} =~ ^[0-9]+$ ]]; then
+  branch=$(${GITHUB_CLI_BIN} api --method GET repos/:owner/:repo/pulls/${DISPATCH_BUILD_NAME#pr-} -q .base.ref)
+else
+  branch=master
+fi
 
 # if we're in dispatch - create the secrets on the container running in the pod with exec
-if [[ ! -z "${CLAIM_NAME}" ]]; then
+if [[ -n "${CLAIM_NAME}" ]]; then
     kubectl --kubeconfig ${KBA_KUBECONFIG} create namespace cert-manager || true
     KIND_POD_NAME=$(kubectl get clusterclaim $CLAIM_NAME | tail +2 | awk '{print $2}')
     kubectl exec -n dispatch $KIND_POD_NAME -- bash -c "docker ps -aq | xargs -I{} docker exec {} cat /etc/kubernetes/pki/ca.crt"  >> ca.crt
@@ -15,7 +18,7 @@ fi
 
 
 echo "INFO: the following test groups will be run:"
-tests=$(go run -tags experimental scripts/test-wrapper.go origin ${branch} | (egrep '^Test' || true) | grep -v 'TestAwsGroup\|TestElasticsearchGroup\|TestIstioGroup')
+tests=$(go run -tags experimental scripts/test-wrapper.go origin ${branch} | (grep -E '^Test' || true) | grep -v 'TestAwsGroup\|TestElasticsearchGroup\|TestIstioGroup')
 echo ${tests}
 
 for g in ${tests}
